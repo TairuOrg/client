76da3ebacc8a1a7771d5724402dc81d731743ae4
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-05-23 13:53:15 -0400

    chore: install tanstack table

---
+++ src/components/tables/cashierTable.tsx
@@ -0,0 +1,6 @@
+import { useReactTable } from '@tanstack/react-table'
+
+import { Table, Thead, Tbody, Tr, Td, Th, TableCaption } from '@chakra-ui/react'
+
+export default function CashierTable() {
+}
