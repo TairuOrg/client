32d41b2d41248591ce516d7a63483f4f83124cea
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-06-02 00:01:06 -0400

    fix: minor visual bugs

--- src/components/landing/Customers.tsx
+++ src/components/landing/Customers.tsx
@@ -1,8 +1,41 @@
+"use client";
 import Logo from "@/assets/Logo";
-import styles from '@/styles/customers.module.css'
+import styles from "@/styles/customers.module.css";
+import { useEffect, useRef, useState } from "react";
 export default function CustomersFeedback() {
+  const [isCustomerVisible, setIsCustomerVisible] = useState(false);
+  const customersRef = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        console.log("observer", entry);
+        setIsCustomerVisible(entry.isIntersecting);
+      },
+      {
+        root: null, // viewport
+        rootMargin: "50px",
+        threshold: 0.5, // trigger when 10% is visible
+      }
+    );
+
+    if (customersRef.current) {
+      observer.observe(customersRef.current);
+    }
+
+    return () => {
+      if (customersRef.current) {
+        observer.unobserve(customersRef.current);
+      }
+    };
+  }, []);
+  console.log(isCustomerVisible)
   return (
-    <section className={styles.customers}>
+    <section
+      className={`${styles.customers} ${
+        isCustomerVisible ? styles.visible : ""
+      }`}
+      ref={customersRef}
+    >
       <h1>
         Nuestros clientes <br />
       </h1>
