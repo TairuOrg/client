3b2e95ec2407eeb2987ce13b702f3d49ceebfa1e
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-06-11 14:08:26 -0400

    chore: Remove unused CSS file and update SVG icons
    This commit removes the unused `footer.module.css` file from the `src/styles` directory. Additionally, the `github.svg` and `instagram.svg` icons in the `public` directory have been updated.
    
    Recent commits indicate that the sign-up view has been worked on, including form validation and submission. There have also been minor changes and improvements made to the form validation and conditional rendering of steps based on search parameters.
    
    Co-authored-by: Juan Barreto <BarretoJuan@users.noreply.github.com>

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
-import styles from "@/styles/footer.module.css";
+
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <section className={styles.socials}>
+    <footer className="flex flex-row w-full h-[415px] bg-teal-800 pt-[100px] justify-evenly text-[30px] text-teal-100">
+      <section className="flex flex-col gap-2 text-[30px]">
         <h1>Redes sociales</h1>
-        <span className="flex flex-row gap-2">
-          <Link
-            href="https://github.com/TauriOrg"
-            className={styles.social_logo_container}
-          >
+        <span className="flex flex-row gap-4 justify-center">
+          <Link href="https://github.com/TauriOrg">
             <Image
               src="/instagram.svg"
               alt="Logo de instagram"
@@ -18,21 +15,7 @@ export default function Footer() {
               width={40}
             />
           </Link>
-          <Link
-            href="https://github.com/TauriOrg"
-            className={styles.social_logo_container}
-          >
-            <Image
-              src="/facebook.svg"
-              alt="Logo de facebook"
-              height={40}
-              width={40}
-            />
-          </Link>
-          <Link
-            href="https://github.com/TauriOrg"
-            className={styles.social_logo_container}
-          >
+          <Link href="https://github.com/TauriOrg">
             <Image
               src="/github.svg"
               alt="Logo de github"
@@ -42,19 +25,21 @@ export default function Footer() {
           </Link>
         </span>
       </section>
-      <section className={styles.policy}>
+      <section>
         <h1>Política de privacidad</h1>
-        <Link href="/sponsors">
-          <h1>Aliados / Sponsors</h1>
-        </Link>
-        <Link href="/policy#data-usage">
-          <h1>Uso de datos</h1>
-        </Link>
-        <Link href="/policy#cookies">
-          <h1>Política de cookies</h1>
-        </Link>
+        <span className="flex flex-col">
+          <Link href="/sponsors">
+            <h1>Aliados / Sponsors</h1>
+          </Link>
+          <Link href="/policy#data-usage">
+            <h1>Uso de datos</h1>
+          </Link>
+          <Link href="/policy#cookies">
+            <h1>Política de cookies</h1>
+          </Link>
+        </span>
       </section>
-      <section className={styles.contact}>
+      <section>
         <h1>Contáctanos</h1>
         <span className={"phone"}> +58 999 999 9999</span>
       </section>
