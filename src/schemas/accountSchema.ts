548c623ca67b421975b88163403893a45c98a440
Juan Barreto <juan2.manuelbarreto@gmail.com>
2024-06-21 17:19:29 -0400

    chore: email input validation

--- src/schemas/accountSchema.ts
+++ src/schemas/accountSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const accountSchema = z.object({
-  email: z.string().email('Correo electrónico inválido'),
+  email: z.string().email('Correo electrónico inválido').min(5,"El correo electrónico debe tener al menos 5 caracteres").max(50, "El correo electrónico no puede tener más de 50 caracteres"),
   password: z.string().min(8, 'La contraseña debe tener al menos 8 caracteres'),
   confirmPassword: z.string()
     .min(8, 'La contraseña debe tener al menos 8 caracteres')
