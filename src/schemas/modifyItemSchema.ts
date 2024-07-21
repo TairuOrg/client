560574ea71f65a13cdb205fa20dab7fdc6470eb8
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-07-20 22:02:35 -0400

    refactor: Update modifyItemSchema to validate positive prices and stock quantities

--- src/schemas/modifyItemSchema.ts
+++ src/schemas/modifyItemSchema.ts
@@ -6,13 +6,14 @@ export const modifyItemSchema = (maxStock: number) =>
       .string()
       .min(1, "El nombre del artículo no puede estar vacío")
       .max(70, "El nombre del artículo no puede tener más de 50 caracteres"),
-    price: z
+      price: z
       .string()
       .min(1, "El precio del artículo no puede estar vacío")
       .regex(
-        /^\d{1,4}(\.\d{1,2})?$/,
-        "El precio no cumple con el formato deseado de hasta 4 dígitos enteros y 2 decimales"
+        /^(?!-)\d{1,4}(\.\d{1,2})?$/,
+        "El precio no cumple con el formato deseado de hasta 4 dígitos enteros y 2 decimales y no puede ser negativo"
       ),
+    
     barcode_id: z
       .string()
       .min(4, "El código de barras debe tener al menos 4 dígitos")
@@ -24,7 +25,7 @@ export const modifyItemSchema = (maxStock: number) =>
     quantity: z
       .string()
       .min(1, "El stock del artículo no puede estar vacío")
-      .refine((currentStock: string) => parseInt(currentStock) <= maxStock, {
+      .refine((currentStock: string) => parseInt(currentStock) <= maxStock && parseInt(currentStock) >= 0, {
         message: `El stock no puede ser mayor al stock máximo (${maxStock})`,
       }),
     manufacturer: z
