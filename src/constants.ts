4f2aa1801063ab96b5d6b839fdb6386320c12ef1
BarretoJuan <juan2.manuelbarreto@gmail.com>
2026-04-08 13:21:28 -0400

    chore: .env/template, and .env constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,7 @@
-export const BASE_URL = "https://smart-quality-antelope.ngrok-free.app";
-export const HOST = "https://tairu.vercel.app"
+
+
+// export const BASE_URL = "https://smart-quality-antelope.ngrok-free.app";
+// export const HOST = "https://tairu.vercel.app"
+
+export const BASE_URL = process.env.BASE_URL;
+export const HOST = process.env.HOST
